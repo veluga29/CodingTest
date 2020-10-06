f4581f83108944d09933f520a05a178669b80b03
veluminous <sungyoon606@naver.com>
2020-10-06 17:41:50 +0900

    Stack practice

---
+++ DataStructure/[Practice] Stack.py
@@ -0,0 +1,15 @@
+# Stack으로 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제() 구현
+
+stack = []
+
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+# 가장 윗 부분부터 출력
+print(stack[::-1])
