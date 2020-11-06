e070c2f3e7e7ec939d47e38b7901508de2f3515c
veluminous <sungyoon606@naver.com>
2020-11-06 15:21:02 +0900

    Refactoring Problem 3

--- [온라인 저지] 실전 문제/우테코 연습 문제/Problem 3 - Implementation.py
+++ [온라인 저지] 실전 문제/우테코 연습 문제/Problem 3 - Implementation.py
@@ -1,4 +1,19 @@
+# 내 풀이
 from functools import reduce
+
+
+# 숫자를 각각의 자리수를 담은 배열로 리턴하는 함수
+def make_nums(num):
+    return [list(map(int, str(num[page]))) for page in range(2)]
+
+
+# 왼쪽 및 오른쪽 페이지 각각의 덧셈 값과 곱셈값을 구해, 그 중 최댓값을 구하는 함수
+def max_among_plus_mul(nums_array):
+    l_plus, r_plus = map(sum, nums_array)
+    l_mul = reduce(lambda x, y: x * y, nums_array[0])
+    r_mul = reduce(lambda x, y: x * y, nums_array[1])
+
+    return max(l_plus, r_plus, l_mul, r_mul)
 
 
 def solution(pobi, crong):
@@ -7,24 +22,13 @@ def solution(pobi, crong):
             pobi[0] % 2 != 1 or pobi[1] % 2 != 0 or crong[0] % 2 != 1 or crong[1] % 2 != 0):
         return -1
 
-    pobi_nums = []  # pobi의 각 페이지의 자리수를 담을 배열
-    crong_nums = []  # crong의 각 페이지의 자리수를 담을 배열
-    # pobi와 crong의 각 페이지마다 자리수를 나눠 리스트화해 pobi_nums, crong_nums에 저장
-    for page in range(2):
-        pobi_nums.append(list(map(int, str(pobi[page]))))
-        crong_nums.append(list(map(int, str(crong[page]))))
+    # pobi와 crong의 각 페이지의 자리수를 담는 배열 구하기
+    pobi_nums = make_nums(pobi)
+    crong_nums = make_nums(crong)
 
-    # pobi의 왼쪽 및 오른쪽 페이지 각각의 덧셈 값과 곱셈값을 구해, 그 중 최댓값을 구함
-    l_plus, r_plus = map(sum, pobi_nums)
-    l_mul = reduce(lambda x, y: x * y, pobi_nums[0])
-    r_mul = reduce(lambda x, y: x * y, pobi_nums[1])
-    pobi_max = max(l_plus, r_plus, l_mul, r_mul)
-
-    # crong의 왼쪽 및 오른쪽 페이지 각각의 덧셈 값과 곱셈값을 구해, 그 중 최댓값을 구함
-    l_plus, r_plus = map(sum, crong_nums)
-    l_mul = reduce(lambda x, y: x * y, crong_nums[0])
-    r_mul = reduce(lambda x, y: x * y, crong_nums[1])
-    crong_max = max(l_plus, r_plus, l_mul, r_mul)
+    # pobi와 crong 각각의 최댓값 구하기
+    pobi_max = max_among_plus_mul(pobi_nums)
+    crong_max = max_among_plus_mul(crong_nums)
 
     if pobi_max > crong_max:  # pobi가 이겼다면
         answer = 1
