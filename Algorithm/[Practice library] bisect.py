948532a5db4563963f2057a3fb50c886ff528161
veluminous <sungyoon606@naver.com>
2020-11-12 22:30:41 +0900

    bisect library practice

---
+++ Algorithm/[Practice library] bisect.py
@@ -0,0 +1,7 @@
+from bisect import bisect_left, bisect_right
+
+a = [1, 2, 4, 4, 8]
+x = 4
+
+print(bisect_left(a, x))  # 정렬된 순서를 유지하면서 배열 a에 x를 삽입할 가장 왼쪽 인덱스를 반환
+print(bisect_right(a, x))  # 정렬된 순서를 유지하면서 배열 a에 x를 삽입할 가장 오른쪽 인덱스를 반환
