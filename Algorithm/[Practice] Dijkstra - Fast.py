24edacc84e9083eb82597f1f47e9310b694ed628
veluminous <sungyoon606@naver.com>
2020-12-19 01:42:24 +0900

    Dijkstra Fast Version practice

---
+++ Algorithm/[Practice] Dijkstra - Fast.py
@@ -0,0 +1,75 @@
+# input
+# 6 11
+# 1
+# 1 2 2
+# 1 3 5
+# 1 4 1
+# 2 3 3
+# 2 4 2
+# 3 2 3
+# 3 6 5
+# 4 3 3
+# 4 5 1
+# 5 3 1
+# 5 6 2
+
+# output
+# 0
+# 2
+# 3
+# 1
+# 2
+# 4
+
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)  # 무한을 의미하는 값으로 10억을 설정
+
+# 노드의 개수, 간선의 개수를 입력 받기
+n, m = map(int, input().split())
+# 시작 노드 번호 입력 받기
+start = int(input())
+# 각 노드에 인접한 노드 정보를 담는 리스트 만들기
+graph = [[] for _ in range(n + 1)]
+# 최단 거리 테이블 값을 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보를 입력 받기
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))  # a번 노드에서 b번 노드로 가는 비용이 c
+
+
+def dijkstra(start):
+    q = []
+    # 시작 노드로 가는 최단 경로는 0으로 설정하고 큐에 삽입
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:  # 큐가 빌 때까지
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(q)
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+        # 현재 노드와 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            # 현재 노드를 경유해 다른 노드로 가는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+# 다익스트라 알고리즘 수행
+dijkstra(start)
+
+# 모든 노드에 대해 최단 거리 출력
+for i in range(1, n + 1):
+    # 도달할 수 없는 경우 무한(INFINITY)으로 출력
+    if distance[i] == INF:
+        print("INFINITY")
+    # 도달할 수 있는 경우 거리를 출력
+    else:
+        print(distance[i])
